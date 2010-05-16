91181d0e53aec12a6a8582a73a2b8610332c3874
yongzhio <yongzhio@cc7fd1d6-d07a-be17-c6a8-8220dac69fb3>
2010-05-16 15:57:53 +0000

    baseline before trial run

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include "ray.h"
 #include "bih.h"
 #include "raytracer.h"
+#include "vec.h"
+#include "color.h"
 
 using namespace std;
 
@@ -15,6 +17,15 @@ int main( int argc, const char* argv[] )
     //Load mesh from file
     Object* targetObject = new Object();
     MLoader::loadM("/home/yzong/Dissertation/Models/models/lion.m",targetObject);
+    
+    Color l_aColor(.2f,.2f,.2f);
+    Color l_dColor(.6f,.6f,.6f);
+    Color l_sColor(.2f,.2f,.2f);
+    double opa = 0.4;
+    double ref = 0.5;
+    double idx = 1.5;
+    double shi = 2.0;
+    targetObject->setMaterial(l_aColor, l_dColor, l_sColor, shi, ref, opa, idx);
 	//MLoader::loadM("C:\\models\\lion.m",targetObject);
     //targetObject->toConsole();
 
@@ -22,7 +33,10 @@ int main( int argc, const char* argv[] )
     Image* image = new Image(500,300);
     Scene* scene = new Scene();
     scene->addObject(targetObject); //ownership of target object taken by scene
-    scene->setCamera(Vec(15,10,5),Vec(-2,2.5,0),Vec(0,1,0),5,(3.1427/3.0f)); //eye,lookat,up,pnear,fovx    
+    scene->setCamera(Vec(15,10,5),Vec(-2,2.5,0),Vec(0,1,0),5,(3.1427/3.0f)); //eye,lookat,up,pnear,fovx
+    Vec light1Location(2.0f,7.0f,6.0f);
+    Color light1Color(1.0f,1.0f,1.0f);
+    scene->setLight(light1Color, light1Location);
     
     //Generate rays
     Ray* rays = Ray::generateRays(*scene, image);
