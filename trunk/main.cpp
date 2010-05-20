cf8d699ca547a7250fd0d303801236979666163f
yongzhio <yongzhio@cc7fd1d6-d07a-be17-c6a8-8220dac69fb3>
2010-05-20 13:10:06 +0000

    added timer

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include "mloader.h"
 #include "image.h"
 #include "scene.h"
@@ -15,9 +16,14 @@ int main( int argc, const char* argv[] )
 	printf( "\nRay Tracing - BIH\n\n" );
 
     //Load mesh from file
+                                                                                                clock_t startLoad=clock();//printf("%d\n",startLoad); 
     Object* targetObject = new Object();
     MLoader::loadM("/home/yzong/Dissertation/Models/models/lion.m",targetObject);
     //MLoader::loadM("C:\\models\\lion.m",targetObject);
+                                                                                                clock_t endLoad=clock()-startLoad;//printf("%d\n",endLoad); 
+                                                                                                printf("Load (C.Cycles): %d\n",endLoad);
+                                                                                                printf("Load (Time    ): %d\n", endLoad / CLOCKS_PER_SEC);
+
 	
     Color l_aColor(.2f,.2f,.2f);
     Color l_dColor(.6f,.6f,.6f);
@@ -29,7 +35,8 @@ int main( int argc, const char* argv[] )
     targetObject->setMaterial(l_aColor, l_dColor, l_sColor, shi, ref, opa, idx);
 	
     //targetObject->toConsole();
-
+        
+                                                                                                clock_t startInitScene=clock();//printf("%d\n",startLoad); 
     //Initialize scene    
     Image* image = new Image(500,300);
     Scene* scene = new Scene();
@@ -38,7 +45,12 @@ int main( int argc, const char* argv[] )
     Vec light1Location(2.0f,7.0f,6.0f);
     Color light1Color(1.0f,1.0f,1.0f);
     scene->setLight(light1Color, light1Location);
+                                                                                                clock_t endInitScene=clock()-startInitScene;//printf("%d\n",endLoad); 
+                                                                                                printf("Load (C.Cycles): %d\n",endInitScene);
+                                                                                                printf("Load (Time    ): %d\n", endInitScene / CLOCKS_PER_SEC);
     
+
+                                                                                                clock_t startGenerate=clock();//printf("%d\n",startLoad); 
     //Generate rays
     Ray* rays = Ray::generateRays(*scene, image);
     /*
@@ -48,14 +60,26 @@ int main( int argc, const char* argv[] )
         printf("%d ",i);
         rays[i].toConsole();
     }*/
+                                                                                                clock_t endGenerate=clock()-startGenerate;//printf("%d\n",endLoad); 
+                                                                                                printf("Gen (C.Cycles): %d\n",endGenerate);
+                                                                                                printf("Gen (Time    ): %d\n", endGenerate / CLOCKS_PER_SEC);  
     
     
+                                                                                                clock_t startBIH=clock();//printf("%d\n",startLoad); 
     //Generate accel struc - bih
     Bih* bih = new Bih();
     bih->buildBIH(scene);
+                                                                                                clock_t endBIH=clock()-startBIH;//printf("%d\n",endLoad); 
+                                                                                                printf("BIH (C.Cycles): %d\n",endBIH);
+                                                                                                printf("BIH (Time    ): %d\n", endBIH / CLOCKS_PER_SEC);  
 
+
+                                                                                                clock_t startTrace=clock();//printf("%d\n",startLoad); 
     //Ray trace
     RayTracer::rayTrace(scene,image,bih->tree(),rays);
+                                                                                                clock_t endTrace=clock()-startTrace;//printf("%d\n",endLoad); 
+                                                                                                printf("Trace (C.Cycles): %d\n",endTrace);
+                                                                                                printf("Trace (Time    ): %d\n", endTrace / CLOCKS_PER_SEC); 
 
     //Export image result
     image->exportImage("result.jpg");
