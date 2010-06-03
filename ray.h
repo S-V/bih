06248d9d2a8bd35a639129af8f1823906d725a3f
yongzhio <yongzhio@cc7fd1d6-d07a-be17-c6a8-8220dac69fb3>
2010-06-03 15:27:52 +0000

    Unrolled and vectorized ray generation loops

--- ray.h
+++ ray.h
@@ -22,34 +22,176 @@ public:
         
 	    double imageWidth = 2.0f * scene.getNear() * tan(scene.getFovx()/2.0f);  // Width of image in world coords
 	    double pixelWidth = imageWidth/image->width();
-
-        //printf("here1");
         
 	    // Image axes in world coordinates
 	    Vec view = !(scene.getLookat() - scene.getEye());	//view vector
 	    Vec u = !(view % scene.getUp());                    //image x-axis (u) = view vector cross prod. with scene up vector
 	    Vec v = !(u % view);                                //image y-axis (v) = image x-axis (u) cross prod. with view vector
-
-        //printf("here2");
+	    
+	    //Pre calculate scene eye + (view * scene.getNear())
+	    Vec l_eyeViewNear = scene.getEye() + view * (float)(scene.getNear());
+	    
+	    //Pre calculate half image width and height
+	    Vec l_halves(image->width(),image->height(),0);
+	    l_halves = l_halves * 0.5;
         
 	    // Trace a ray through every pixel
-	    for (int i=0; i<image->height(); i++) {
+	    for (int i=0; i<image->height(); i+=4) { //loop unrolled
 
-		    for (int j=0; j<image->width(); j++) {
+	    	Vec l_iMaster(i,i+1,i+2,i+3);
+	    	
+		    for (int j=0; j<image->width(); j+=4) { //loop unrolled
 
 			    // Find position of pixel in world coordinates
-			    Vec worldPix = scene.getEye() 
+		    	//Rolled
+			    /*Vec worldPix = scene.getEye() 
 				      + view * (float)(scene.getNear())
 				      + u*( (j-(image->width()/2.0f))*(float)pixelWidth )
 				      + v*( (i-(image->height()/2.0f))*(float)pixelWidth );
+				*/
+		    	
+		    	//Rolled
+		    	/*Vec worldPix = l_eyeViewNear
+		    				  + u*( (j-(l_halves.data.fVals[0]))*(float)pixelWidth )
+    					      + v*( (i-(l_halves.data.fVals[1]))*(float)pixelWidth );*/
+		    	
+			    //Rolled
+		    	//Vec direction = worldPix - scene.getEye();
+		    	
+		    	//Rolled
+			    //direction = !direction;
+		    	
+		    	//Rolled
+                //allRays[(i*image->width())+j].set(direction); 
+		    	
+		    	//Unrolled
+		    	Vec l_jMaster(j,j+1,j+2,j+3);
+		    	
+		    	Vec l_halfWidths(l_halves[0]);
+		    	Vec l_j = l_jMaster - l_halfWidths;
+		    	
+		    	Vec l_halfHeights(l_halves[1]);
+		    	Vec l_i = l_iMaster - l_halfHeights;
+		    	
+		    	
+		    	//Unrolled
+		    	Vec worldPix11 = l_eyeViewNear
+		    			    				  + u*( l_j[0] * (float)pixelWidth )
+		    	    					      + v*( l_i[0]   *(float)pixelWidth );
+		    	Vec worldPix12 = l_eyeViewNear
+		    			    				  + u*( l_j[0] * (float)pixelWidth )
+		    	    					      + v*( l_i[1]   *(float)pixelWidth );
+		    	Vec worldPix13 = l_eyeViewNear
+		    			    				  + u*( l_j[0] * (float)pixelWidth )
+		    	    					      + v*( l_i[2]   *(float)pixelWidth );
+		    	Vec worldPix14 = l_eyeViewNear
+		    			    				  + u*( l_j[0] * (float)pixelWidth )
+		    	    					      + v*( l_i[3]   *(float)pixelWidth );
+		    	
+		    	Vec worldPix21 = l_eyeViewNear
+		    			    				  + u*( l_j[1] *(float)pixelWidth )
+		    	    					      + v*( l_i[0]    *(float)pixelWidth );
+		    	Vec worldPix22 = l_eyeViewNear
+		    			    				  + u*( l_j[1] *(float)pixelWidth )
+		    	    					      + v*( l_i[1]    *(float)pixelWidth );
+		    	Vec worldPix23 = l_eyeViewNear
+		    			    				  + u*( l_j[1] *(float)pixelWidth )
+		    	    					      + v*( l_i[2]    *(float)pixelWidth );
+		    	Vec worldPix24 = l_eyeViewNear
+		    			    				  + u*( l_j[1] *(float)pixelWidth )
+		    	    					      + v*( l_i[3]    *(float)pixelWidth );
+		    	
+		    	Vec worldPix31 = l_eyeViewNear
+		    			    				  + u*( l_j[2] *(float)pixelWidth )
+		    	    					      + v*( l_i[0]    *(float)pixelWidth );
+		    	Vec worldPix32 = l_eyeViewNear
+		    			    				  + u*( l_j[2] *(float)pixelWidth )
+		    	    					      + v*( l_i[1]    *(float)pixelWidth );
+		    	Vec worldPix33 = l_eyeViewNear
+		    			    				  + u*( l_j[2] *(float)pixelWidth )
+		    	    					      + v*( l_i[2]    *(float)pixelWidth );
+		    	Vec worldPix34 = l_eyeViewNear
+		    			    				  + u*( l_j[2] *(float)pixelWidth )
+		    	    					      + v*( l_i[3]    *(float)pixelWidth );
+		    	
+		    	Vec worldPix41 = l_eyeViewNear
+		    			    				  + u*( l_j[3] *(float)pixelWidth )
+		    	    					      + v*( l_i[0]    *(float)pixelWidth );
+		    	Vec worldPix42 = l_eyeViewNear
+		    			    				  + u*( l_j[3] *(float)pixelWidth )
+		    	    					      + v*( l_i[1]    *(float)pixelWidth );
+		    	Vec worldPix43 = l_eyeViewNear
+		    			    				  + u*( l_j[3] *(float)pixelWidth )
+		    	    					      + v*( l_i[2]    *(float)pixelWidth );
+		    	Vec worldPix44 = l_eyeViewNear
+		    			    				  + u*( l_j[3] *(float)pixelWidth )
+		    	    					      + v*( l_i[3]    *(float)pixelWidth );
+			    
+		    	// Direction of ray (note: vector is not normalized)
+		    	//Unrolled
+		    	Vec direction11 = worldPix11 - scene.getEye();
+		    	Vec direction12 = worldPix12 - scene.getEye();
+		    	Vec direction13 = worldPix13 - scene.getEye();
+		    	Vec direction14 = worldPix14 - scene.getEye();
+		    	
+		    	Vec direction21 = worldPix21 - scene.getEye();
+		    	Vec direction22 = worldPix22 - scene.getEye();
+		    	Vec direction23 = worldPix23 - scene.getEye();
+		    	Vec direction24 = worldPix24 - scene.getEye();
+		    	
+		    	Vec direction31 = worldPix31 - scene.getEye();
+		    	Vec direction32 = worldPix32 - scene.getEye();
+		    	Vec direction33 = worldPix33 - scene.getEye();
+		    	Vec direction34 = worldPix34 - scene.getEye();
+		    	
+		    	Vec direction41 = worldPix41 - scene.getEye();
+		    	Vec direction42 = worldPix42 - scene.getEye();
+		    	Vec direction43 = worldPix43 - scene.getEye();
+		    	Vec direction44 = worldPix44 - scene.getEye();
 
-			    // Direction of ray (note: vector is not normalized)
-			    Vec direction = worldPix - scene.getEye();
-
-			    direction = !direction; //normalize direction
+		    	//normalize direction
+		    	//Unrolled
+		    	direction11 = !direction11;
+		    	direction12 = !direction12;
+		    	direction13 = !direction13;
+		    	direction14 = !direction14;
+		    	
+		    	direction21 = !direction21;
+		    	direction22 = !direction22;
+		    	direction23 = !direction23;
+		    	direction24 = !direction24;
+		    	
+		    	direction31 = !direction31;
+		    	direction32 = !direction32;
+		    	direction33 = !direction33;
+		    	direction34 = !direction34;
+		    	
+		    	direction41 = !direction41;
+		    	direction42 = !direction42;
+		    	direction43 = !direction43;
+		    	direction44 = !direction44;
 
 			    // Construct the ray
-                allRays[(i*image->width())+j].set(direction); 
+		    	//Unrolled
+		    	allRays[((int)l_iMaster[0] *image->width())+ (int)l_jMaster[0] ].set(direction11);
+		    	allRays[((int)l_iMaster[0] *image->width())+ (int)l_jMaster[1] ].set(direction21);
+		    	allRays[((int)l_iMaster[0] *image->width())+ (int)l_jMaster[2] ].set(direction31);
+		    	allRays[((int)l_iMaster[0] *image->width())+ (int)l_jMaster[3] ].set(direction41);
+		    	
+		    	allRays[((int)l_iMaster[1]*image->width())+ (int)l_jMaster[0] ].set(direction12);
+		    	allRays[((int)l_iMaster[1]*image->width())+ (int)l_jMaster[1] ].set(direction22);
+		    	allRays[((int)l_iMaster[1]*image->width())+ (int)l_jMaster[2] ].set(direction32);
+		    	allRays[((int)l_iMaster[1]*image->width())+ (int)l_jMaster[3] ].set(direction42);
+		    	
+		    	allRays[((int)l_iMaster[2]*image->width())+ (int)l_jMaster[0] ].set(direction13);
+		    	allRays[((int)l_iMaster[2]*image->width())+ (int)l_jMaster[1] ].set(direction23);
+		    	allRays[((int)l_iMaster[2]*image->width())+ (int)l_jMaster[2] ].set(direction33);
+		    	allRays[((int)l_iMaster[2]*image->width())+ (int)l_jMaster[3] ].set(direction43);
+		    	
+		    	allRays[((int)l_iMaster[3]*image->width())+ (int)l_jMaster[0] ].set(direction14);
+		    	allRays[((int)l_iMaster[3]*image->width())+ (int)l_jMaster[1] ].set(direction24);
+		    	allRays[((int)l_iMaster[3]*image->width())+ (int)l_jMaster[2] ].set(direction34);
+		    	allRays[((int)l_iMaster[3]*image->width())+ (int)l_jMaster[3] ].set(direction44);
                 
                 //printf("Ray %d created.\n",i*image->width()+j);
 		    }
